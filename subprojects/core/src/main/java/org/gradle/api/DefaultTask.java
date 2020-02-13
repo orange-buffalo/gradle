f7b0dd175ced94741781a217be263b4dd0e1a8ef
Paul Merlin <paul@gradle.com>
2020-02-14 00:56:18 +0100

    Introduce DefaultTask.getFileSystemOperations()
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/core/src/main/java/org/gradle/api/DefaultTask.java
+++ subprojects/core/src/main/java/org/gradle/api/DefaultTask.java
@@ -16,6 +16,7 @@
 
 package org.gradle.api;
 
+import org.gradle.api.file.FileSystemOperations;
 import org.gradle.api.file.ProjectLayout;
 import org.gradle.api.internal.AbstractTask;
 import org.gradle.api.model.ObjectFactory;
@@ -50,6 +51,18 @@ public class DefaultTask extends AbstractTask {
     @Inject
     @Incubating
     protected ProjectLayout getProjectLayout() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Provides access to various file system operations.
+     *
+     * @return the file system operations
+     * @since 6.3
+     */
+    @Inject
+    @Incubating
+    protected FileSystemOperations getFileSystemOperations() {
         throw new UnsupportedOperationException();
     }
 }
